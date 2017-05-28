270e1d04e54956ff824f476a6566e3e90b1a6f95
Michael418 <smolyakov7@rambler.ru>
2017-05-28 22:34:56 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const webpack = require('webpack');
+const DEV_ENV = true;
+
+module.exports = {
+	entry: {
+		home1: './home1/js/app.js'
+	},
+	output: {
+		path: __dirname + '/src/comp',
+		filename: "[name].js",
+		library: "[name]"
+	},
+
+	watch: true,
+
+	devtool: "cheap-inline-source-map",
+
+	plugins: [
+		new webpack.DefinePlugin({
+			DEV_ENV : DEV_ENV
+ 		}),
+ 	],
+
+	module : {
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			exclude: [/node_modules/],
+		},
+		{
+			test: /\.scss$/,
+			loader: 'style-loader!css-loader!sass-loader',
+			exclude: [/node_modules/],
+		},
+		{
+			test: /\.css$/,
+			loader: 'style-loader!css-loader',
+		}
+		]
+	}
+}
